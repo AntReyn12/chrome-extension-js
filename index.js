d4c9387288585fc99418920f965824b712b31fd5
ant_reyn <antreyn@udel.edu>
2021-10-26 14:50:00 -0400

    small changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+const myLeads = [];
 
 inputBtn.addEventListener("click", function () {
   console.log("Button clicked from event listener");
