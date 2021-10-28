70be7803e33a0f06b890b67026734491233cf966
ant_reyn <antreyn@udel.edu>
2021-10-28 18:10:14 -0400

    added ul and li elements to the app

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
-const myLeads = [];
+const ulEl = document.querySelector("#list-el");
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+
+console.log(ulEl);
 
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked from event listener");
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
 });
+
+for (i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
