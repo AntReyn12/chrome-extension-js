f08eaac7288c5f7c5b3755b2ae56b5a178558180
ant_reyn <antreyn@udel.edu>
2021-10-29 18:21:59 -0400

    minor tweaks

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#list-el");
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 
-console.log(ulEl);
-
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
 });
 
+let listItems = "";
+
 for (i = 0; i < myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+  listItems += "<li>" + myLeads[i] + "</li>";
+  ulEl.innerHTML = listItems;
   //*Another way to create elements*//
   //const li = document.createElement("li");
   //li.textContent += myLeads[i];
