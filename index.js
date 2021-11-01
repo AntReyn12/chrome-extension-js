d737c69f9e3cea6c9fd120dba2cfa3be64d51da0
ant_reyn <antreyn@udel.edu>
2021-11-01 15:53:31 -0400

    opreparing for deployment

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#list-el");
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
 
-let listItems = "";
-
-for (i = 0; i < myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  let listItems = "";
+  for (i = 0; i < myLeads.length; i++) {
+    listItems += `<li> 
+                    <a 
+                      href= "${myLeads[i]}" target="_blank"> ${myLeads[i]} 
+                    </a>
+                  </li>`;
+    console.log(listItems);
+    //*Another way to create elements*//
+    //const li = document.createElement("li");
+    //li.textContent += myLeads[i];
+    //ulEl.append(li);
+  }
   ulEl.innerHTML = listItems;
-  //*Another way to create elements*//
-  //const li = document.createElement("li");
-  //li.textContent += myLeads[i];
-  //ulEl.append(li);
 }
