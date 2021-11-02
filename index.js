8c7cde7467ff85634c0be2b8c42f8f3d2c5728dd
ant_reyn <antreyn@udel.edu>
2021-11-02 18:13:44 -0400

    optomized the code by passing an argument in the render function

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
+const deleteBtn = document.querySelector("#delete-btn");
 const ulEl = document.querySelector("#list-el");
 let myLeads = [];
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (i = 0; i < myLeads.length; i++) {
+  for (i = 0; i < leads.length; i++) {
     listItems += `<li> 
                     <a 
-                      href= "${myLeads[i]}" target="_blank"> ${myLeads[i]} 
+                      href= "${leads[i]}" target="_blank"> ${leads[i]} 
                     </a>
                   </li>`;
     //*Another way to create elements*//
@@ -32,3 +26,16 @@ function renderLeads() {
   }
   ulEl.innerHTML = listItems;
 }
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  myLeads = [];
+  localStorage.clear();
+  render(myLeads);
+});
