820ca7dad67aa349bac3d9523180266e30565133
ant_reyn <antreyn@udel.edu>
2021-11-03 18:19:20 -0400

    minor change

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ function render(leads) {
 
 tabBtn.addEventListener("click", function () {
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-    console.log(tabs);
     myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
